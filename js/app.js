be6b0b9b86531396accb2099047ce9d2c594d9a8
Mutakabbir Yeasin <mutakabbir.yeasin@gmail.com>
2023-03-23 02:31:18 +0600

    destructure option 1 & 2

--- js/app.js
+++ js/app.js
@@ -12,17 +12,40 @@ const displayCountries = (countries) => {
   container.innerHTML = countrisHtml.join(" "); // arrayName.join("")--> cotation er vitore jai deoa hobe ta darai separate hobe. space dile space diye or comma dile comma diye separate hobe.
 };
 
-const getCountry = (country) => {
-  const { name, flags } = country;
+// option 2
+const getCountry = ({ name, flags, area, region }) => {
   return `
-    
+
   <div class="country">
     <h2>${name.common}</h2>
+    <h4>Area: ${area}</h4>
+    <h4>Continent: ${region}</h4>
     <img src="${flags.png}" >
   </div>
-    
-    
     `;
 };
+
+// original
+
+// const getCountry = (country) => {
+//   return `
+//   <div class="country">
+//     <h2>${country.name.common}</h2>
+//     <img src="${country.flags.png}" >
+//   </div>
+//     `;
+// };
+
+// option 1 [destruct]
+// const getCountry = (country) => {
+//   const { name, flags } = country;
+//   return `
+
+//   <div class="country">
+//     <h2>${name.common}</h2>
+//     <img src="${flags.png}" >
+//   </div>
+//     `;
+// };
 
 loadCountries();
